39cbeb17460916d7435cc430d11318adec78c8c9
y-i <y-i@users.noreply.github.com>
2020-10-21 01:53:15 +0900

    update ch-4-ex-1

--- exercises/ch-4-ex-1/protectedResource.js
+++ exercises/ch-4-ex-1/protectedResource.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var bodyParser = require('body-parser');
-var cons = require('consolidate');
-var nosql = require('nosql').load('database.nosql');
-var __ = require('underscore');
-var cors = require('cors');
+const express = require("express");
+const bodyParser = require('body-parser');
+const cons = require('consolidate');
+const nosql = require('nosql').load('database.nosql');
+const __ = require('underscore');
+const cors = require('cors');
 
-var app = express();
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true })); // support form-encoded bodies (for bearer tokens)
 
@@ -17,35 +17,51 @@ app.set('json spaces', 4);
 app.use('/', express.static('files/protectedResource'));
 app.use(cors());
 
-var resource = {
+const resource = {
 	"name": "Protected Resource",
 	"description": "This data has been protected by OAuth 2.0"
 };
 
-var getAccessToken = function(req, res, next) {
-	/*
-	 * Scan for an access token on the incoming request.
-	 */
+const getAccessToken = function(req, res, next) {
+	let inToken = null;
+	const auth = req.headers['authorization'];
+	if (auth && auth.toLowerCase().indexOf('bearer') == 0) { // Authorization header
+		inToken = auth.slice('bearer '.length);
+	} else if (req.body && req.body.access_token) { // form
+		// not in the header, check in the form body
+		inToken = req.body.access_token;
+	} else if (req.query && req.query.access_token) { // query string
+		inToken = req.query.access_token
+	}
 	
+	console.log('Incoming token: %s', inToken);
+	nosql.find().make(builder => {
+		builder.where('access_token', inToken);
+		builder.callback((err, [token]) => {
+			if (token) {
+				console.log("We found a matching token: %s", inToken);
+			} else {
+				console.log('No matching token was found.');
+			}
+			req.access_token = token;
+			next();
+		});
+	});
 };
 
 app.options('/resource', cors());
 
-
-/*
- * Add the getAccessToken function to this handler
- */
-app.post("/resource", cors(), function(req, res){
-
-	/*
-	 * Check to see if the access token was found or not
-	 */
-	
+app.post("/resource", cors(), getAccessToken, function(req, res){
+	if (req.access_token) {
+		res.json(resource);
+	} else {
+		res.status(401).end();
+	}
 });
 
-var server = app.listen(9002, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
+const server = app.listen(9002, 'localhost', function () {
+  const host = server.address().address;
+  const port = server.address().port;
 
   console.log('OAuth Resource Server is listening at http://%s:%s', host, port);
 });
